a1a5a7adf4adba57f61e4fe6cdfca23863bf2d06
mlfbrown <mlfbrown@stanford.edu>
2022-03-16 16:44:17 -0400

    Playing with typed vs untyped ASTs

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -7,34 +7,40 @@
 //!    * AST
 //!
 //! MLFB note: We may to having each enum take a struct for accessor reasons
+use crate::ir::term::Sort; 
+
+/// Temporary 
+pub type PlainStmt = Stmt<()>;
+/// Temporary
+pub type TypedStmt = Stmt<Sort>;
 
 /// A statement node
-pub enum Stmt {
+pub enum Stmt<T> {
     /// An assignment. For now, we only support assignment statements.
     /// In the future, we have a few options:
     /// 1. Keep it that way (and let C AST -> generic AST mappers figure it out)
     /// 2. Keep it that way and support some other flexible expression type like sequences
     /// 3. Support both assignment statements and expressions, but have a check that ensures
     ///    you don't intermix both in the same AST (because that seems...concerning?)
-    AssignStmt(Var, Expr),
+    AssignStmt(Var<T>, Expr<T>),
     /// A return
-    ReturnStmt(Option<Expr>), 
+    ReturnStmt(Option<Expr<T>>), 
     /// An If statement, with optional branch for optional else. 
-    IfStmt(Expr, Box<Stmt>, Option<Box<Stmt>>),
+    IfStmt(Expr<T>, Box<Stmt<T>>, Option<Box<Stmt<T>>>),
     /// A while loop.
-    WhileStmt(Expr, Box<Stmt>),
+    WhileStmt(Expr<T>, Box<Stmt<T>>),
     /// A compound statement (i.e., a list of statements)
-    CompoundStmt(Vec<Box<Stmt>>), 
+    CompoundStmt(Vec<Box<Stmt<T>>>), 
 }
 
 /// An expression node (e.g., x + 5) 
-pub enum Expr {
+pub enum Expr<T> {
     /// A constant (e.g., 5)
     ConstExpr(Literal),
     /// A variable (e.g., x)
-    VarExpr(Var),
+    VarExpr(Var<T>),
     /// A binary expression (e.g., x + 5)
-    BinExpr(BinOp, Box<Expr>, Box<Expr>), 
+    BinExpr(BinOp, Box<Expr<T>>, Box<Expr<T>>), 
 }
 
 /// A binary operator. Note: distinct from comparison 
@@ -50,9 +56,11 @@ pub enum BinOp {
 }
 
 /// A variable (e.g., x) 
-pub struct Var {
+pub struct Var<T> {
     /// A variable name 
-    pub name: String, 
+    pub name: String,
+    /// The variable's metadata (e.g., type)
+    pub meta: T,
 }
 
 /// A literal
