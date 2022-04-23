9d436c5a9ab1b1c7834ea862db81e5c5df79aee6
mlfbrown <mlfbrown@stanford.edu>
2022-04-23 18:44:52 -0400

    function signatures for embeddable

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -7,7 +7,10 @@
 //!    * AST
 //!
 //! MLFB note: We may to having each enum take a struct for accessor reasons
-use crate::ir::term::{Sort, Term, Op, TermData, term};
+//! MLFB note: Say something about the semantics of operators being lower level
+//! (e.g., not wrapping say C shift semantics and insteading following SMT lib)
+use crate::circify::{Circify, Embeddable, CirCtx};
+use crate::ir::term::{Sort, Term, Op, PartyId, term};
 use crate::ir::term::{BV_ADD, BV_SUB, BV_MUL, BV_UDIV};
 
 /// Temporary 
@@ -96,7 +99,7 @@ pub enum BinOp {
     /// *
     MulOp,
     /// / 
-    DivOp, 
+    UDivOp, 
 }
 
 /// A variable (e.g., x) 
@@ -119,25 +122,59 @@ pub enum Literal {
     BoolLit(bool),
 }
 
+/// Generic AST langauge definition
+/// Ask Alex what this is/should be 
+pub struct ASTDef;
+
+impl Embeddable for ASTDef {
+    type T = Term;
+    type Ty = Sort;
+
+    fn declare(
+        &self,
+        ctx: &mut CirCtx,
+        ty: &Self::Ty,	
+	raw_name: String,
+	user_name: Option<String>,
+	visibility: Option<PartyId>,
+    ) -> Self::T { panic! () }
+
+    fn ite(&self, _ctx: &mut CirCtx, cond: Term, t: Self::T, f: Self::T) -> Self::T { panic!() }
+
+    fn assign(
+        &self,
+        ctx: &mut CirCtx,
+        ty: &Self::Ty,
+        name: String,
+        t: Self::T,
+        visibility: Option<PartyId>,
+    ) -> Self::T { panic!() }
+
+    fn values(&self) -> bool { false }
+
+    fn type_of(&self, term: &Self::T) -> Self::Ty { panic!() }
+
+    fn initialize_return(&self, ty: &Self::Ty, _ssa_name: &String) -> Self::T { panic!() }
+}
+
+
 /// Struct for lowering from AST to IR.
 /// It lives here for now but all of this will probably move.
 pub struct ASTGen {
-    /// Dummy
-    pub dummy: u32,
+//    circ: Circify<ASTDef>, 
 }
 
 impl ASTGen {
-
-    /// Make a new lowering struct (ASTGen) 
-    pub fn new() -> Self {
-	ASTGen { dummy: 32 }
-    }
 
     /// Lower a translation unit from AST to IR
     pub fn lower_trans_unit<T: Typed> (trans_unit: TransUnit<T>) -> () {
 
     }
 
+    pub fn lower_stmt<T: Typed> (stmt: Stmt<T>) -> () {
+	
+    }
+    
     /// Lower an expression from AST to IR
     fn lower_expr<T: Typed>(&self, expr: Expr<T>) -> Term {
 	match expr {
@@ -146,19 +183,20 @@ impl ASTGen {
 		let right_term = self.lower_expr(*right);
 		let args = vec![left_term, right_term];
 		match op {
-		    AddOp => term(BV_ADD, args),
-		    SubOp => term(BV_SUB, args),
-		    MulOp => term(BV_MUL, args),
-		    DivOp => term(BV_UDIV, args), 
+		    BinOp::AddOp => term(BV_ADD, args),
+		    BinOp::SubOp => term(BV_SUB, args),
+		    BinOp::MulOp => term(BV_MUL, args),
+		    BinOp::UDivOp => term(BV_UDIV, args), 
 		}
 	    }
+	    Expr::VarExpr(v) => self.lower_var(v), 
 	    _ => unimplemented!()
 	}
     }
 
     fn lower_var<T: Typed>(&self, v: Var<T>) -> Term {
 	let sort = v.get_type();
-	panic!()
+	term(Op::Var(v.name, sort), vec![])
     }
 
 }
